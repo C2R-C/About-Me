3728cd13a7ab1bfda21a499f8188abd502268101
Robinnsson Cifuentes Ramírez <robinnsson@gmail.com>
2020-12-16 18:04:45 -0500

    Desarrollo del codelab lección 2.2

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 Esta es una app que elaboro con el propósito de ejercitarme con los codelabs de Google Android Studio.
 
 En este repositorio desarrollaré cada una de las actividades propuestas en los codelabs para esta app.
+
+## Task codelab: Agregar interactividad del usuario
+
+
+1. ¿Qué es una `EditText` subclase de?
+    Respuesta: `TextView`
+
+2. ¿Cuál de los siguientes valores del atributo `visibility`, que si se establece en una vista, hace que la vista esté oculta y no ocupe espacio en el diseño?
+    Respuesta: gone
+
+3. Para las vistas `EditText`, no es una buena práctica proporcionar sugerencias, porque las sugerencias saturan el campo de entrada. ¿Verdadero o falso?
+    Respuesta: Falso
+
+4. ¿Cuál de las siguientes afirmaciones es verdadera sobre las opiniones `Button`?
+    Respuesta: `Button` se puede hacer clic en las vistas y, al hacer clic, el oyente de clics adjunto realiza una acción.
--- app/src/main/java/com/c2r/aboutme/MainActivity.kt
+++ app/src/main/java/com/c2r/aboutme/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.c2r.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.view.isGone
+
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var nicknameEdit : EditText
+    lateinit var nicknameText : TextView
+    lateinit var doneButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nicknameEdit = findViewById(R.id.et_nickname)
+        nicknameText = findViewById(R.id.nickname_text)
+        doneButton = findViewById(R.id.done_button)
+
+        doneButton.setOnClickListener { addNewNickname(it) }
+        nicknameText.setOnClickListener { changeNickname(it) }
+
     }
+
+    private fun addNewNickname(view: View) {
+
+        nicknameText.text = nicknameEdit.text
+        nicknameEdit.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+        // Hide the keyboard.
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+
+    }
+
+    fun changeNickname(view: View) {
+
+        view.visibility = View.GONE
+        nicknameEdit.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        nicknameEdit.requestFocus()
+        // Show the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(nicknameEdit, 0)
+
+    }
+
+
 }
